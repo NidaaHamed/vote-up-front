e860d09d21023856ab23784173785ab518c84c18
nidaa <nidaasallam@gmail.com>
2021-07-21 15:39:54 +0200

    form to add posts

---
+++ src/components/Posts.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container p-5">
+        <b-form @submit="addPost">
+            <b-form-textarea
+                id="textarea"
+                v-model="post"
+                placeholder="Enter something..."
+                rows="3"
+                max-rows="6"
+            ></b-form-textarea>
+            <b-form-file
+                v-model="images"
+                placeholder="Choose a img or drop it here..."
+                drop-placeholder="Drop img here..."
+                accept="image/*"
+                class="mt-3"
+            ></b-form-file>
+            <b-button type="submit" variant="success" class="mt-3">Add Post</b-button>
+        </b-form>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            post: '',
+            userId: '',
+            images: []
+        };
+    },
+    methods: {
+        addPost(event) {
+            event.preventDefault();
+            console.log(this.post);
+            axios({
+                method: 'post',
+                url: 'http://graduationproject1.zahran4it.com/api/Posts/AddPost',
+                data: {
+                    id: '',
+                    text: this.post,
+                    userId: this.userId,
+                    images: ['']
+                }
+            }).then(res => {
+                console.log(res.data);
+            });
+        },
+    },
+    created() {
+        let userdata = JSON.parse(localStorage.getItem("user"));
+        this.userId = userdata.data.user.id;
+        console.log("user id : "+this.userId);
+    },
+
+}
+</script>
